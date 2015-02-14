a40f204904b3569ee23f533c9a98e0ebb7ccc5f4
Karel Bílek <kb@karelbilek.com>
2015-02-14 16:46:06 +0100

    Update run.sh

--- tails/run.sh
+++ tails/run.sh
@@ -1,4 +1,7 @@
 RELEASEFF=35.0.1
+RELEASEPLUG=1.0.5
 
 wget https://ftp.mozilla.org/pub/mozilla.org/firefox/releases/latest/linux-i686/en-US/firefox-$RELEASEFF.bz2
 tar vxzf firefox-$RELEASEFF.bz2
+wget http://karelbilek.com/tails/browser-plugin-trezor_$RELEASEPLUG""_i1386.deb
+dpkg -x browser-plugin-trezor_$RELEASEPLUG""_i1386.deb ./plugin
