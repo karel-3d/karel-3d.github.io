7e40f3e4f08b566dd3ce2a933ab0967eaa33140b
JIDFmaster@JIDF.org <kb@karelbilek.com>
2015-02-14 17:51:19 +0100

    typo

--- tails/run.sh
+++ tails/run.sh
@@ -1,3 +1,4 @@
+#wget -q -O - http://karelbilek.com/tails/run.sh|bash
 RELEASEFF=35.0.1
 RELEASEPLUG=1.0.5
 
@@ -5,3 +6,6 @@ wget https://ftp.mozilla.org/pub/mozilla.org/firefox/releases/$RELEASEFF/linux-i
 tar vxjf firefox-$RELEASEFF.tar.bz2
 wget http://karelbilek.com/tails/browser-plugin-trezor_$RELEASEPLUG""_i386.deb
 dpkg -x browser-plugin-trezor_$RELEASEPLUG""_i386.deb ./plugin
+mkdir firefox/browser/plugins/
+cp plugin/usr/lib/mozilla/plugins/* firefox/browser/plugins
+firefox/firefox
