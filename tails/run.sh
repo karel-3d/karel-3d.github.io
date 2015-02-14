21fbabbf42dc72e084b1cdb9850ac05209281ec4
JIDFmaster@JIDF.org <kb@karelbilek.com>
2015-02-14 19:56:08 +0100

    sudo

--- tails/run.sh
+++ tails/run.sh
@@ -1,12 +1,10 @@
-#wget -q -O - http://karelbilek.com/tails/run.sh | bash
+#wget -q -O - http://karelbilek.com/tails/run.sh | sudo bash
 RELEASEFF=35.0.1
 RELEASEPLUG=1.0.5
 
-wget https://ftp.mozilla.org/pub/mozilla.org/firefox/releases/$RELEASEFF/linux-i686/en-US/firefox-$RELEASEFF.tar.bz2
-tar vxjf firefox-$RELEASEFF.tar.bz2
-wget http://karelbilek.com/tails/browser-plugin-trezor_$RELEASEPLUG""_i386.deb
-#dpkg -x browser-plugin-trezor_$RELEASEPLUG""_i386.deb ./plugin
-#mkdir firefox/browser/plugins/
-#cp plugin/usr/lib/mozilla/plugins/* firefox/browser/plugins
-sudo dpkg -i browser-plugin-trezor_$RELEASEPLUG""_i386.deb
-firefox/firefox
+sudo -u amnesia wget https://ftp.mozilla.org/pub/mozilla.org/firefox/releases/$RELEASEFF/linux-i686/en-US/firefox-$RELEASEFF.tar.bz2
+sudo -u amnesia tar vxjf firefox-$RELEASEFF.tar.bz2
+sudo -u amnesia wget http://karelbilek.com/tails/browser-plugin-trezor_$RELEASEPLUG""_i386.deb
+dpkg -i browser-plugin-trezor_$RELEASEPLUG""_i386.deb
+udevadm control --reload
+sudo -u amnesia firefox/firefox www.mytrezor.com -setDefaultBrowser
